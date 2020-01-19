c7a7083f0a4e48baf8efc5a77ec0db0ec43fa112
matheusloures <matheusloures@gmail.com>
2020-01-19 04:35:34 -0800

    first

---
+++ index.js
@@ -0,0 +1,74 @@
+const readline = require('readline');
+var utils = require('coloringautils')
+const clear = require('clear');
+const figlet = require('figlet');
+const fs = require('fs');
+const files = require('./lib/files');
+const path = require('path');
+
+clear();
+
+console.log(
+ 
+    figlet.textSync('COLORDEV', { horizontalLayout: 'full' })
+  
+);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (input) => {
+    // console.log(`Received: ${input}`);
+});
+
+function write(what,where){
+    return new Promise((resolve, reject)=>{
+
+        fs.writeFile(where, what, function(err) {
+            if(err) {
+                reject()
+            }
+            resolve()
+        }); 
+    })
+}
+
+  
+
+rl.question('Do what? ', (answer) => {
+    // var page1 = require('./indexroot.js')
+    // var stringify = JSON.parse(page1)
+  write(`
+  var utils = require('coloringautils');
+  var write = require('coloringautils/writefile');
+  var colorxjs = require('colorxjs');
+  `, 
+  path.resolve(__dirname, './test.js')).then(res=>{
+
+            write(`
+            {
+                "name": "colordummy",
+                "version": "1.0.0",
+                "description": "change me",
+                "main": "index.js",
+                "scripts": {
+                  "test": "echo \"Error: no test specified\" && exit 1"
+                },
+                "author": "Matheus Loures",
+                "license": "ISC"
+            };
+            `, 
+            path.resolve(__dirname, './package.json')).then(res=>{
+
+                console.log("See Yaaaaa!");
+            },err=>{
+                console.log("Deu ruim no finalzinho");
+            })
+  },err=>{
+    console.log("Deu ruim");
+  })
+  
+  rl.close();
+})
